b6b31cd93a71d0f5b0fe97fdba5e2bc10dc63328
milsolecba@gmail.com <milsolecba@gmail.com@c78c6a92-93a8-c6d3-608b-43237b1af5e0>
2011-07-06 22:58:42 +0000

    agregando modulos by sole

---
+++ trunk/sites/all/modules/bajar.sh
@@ -0,0 +1,42 @@
+wget http://ftp.drupal.org/files/projects/cck-6.x-2.9.tar.gz
+tar xvzf cck-6.x-2.9.tar.gz
+rm cck-6.x-2.9.tar.gz
+wget http://ftp.drupal.org/files/projects/fb_social-6.x-1.0-beta8.tar.gz
+tar xvzf fb_social-6.x-1.0-beta8.tar.gz
+rm fb_social-6.x-1.0-beta8.tar.gz
+wget http://ftp.drupal.org/files/projects/filefield-6.x-3.10.tar.gz
+tar xvzf filefield-6.x-3.10.tar.gz
+rm filefield-6.x-3.10.tar.gz
+wget http://ftp.drupal.org/files/projects/imageapi-6.x-1.10.tar.gz
+tar xvzf imageapi-6.x-1.10.tar.gz
+rm imageapi-6.x-1.10.tar.gz
+wget http://ftp.drupal.org/files/projects/imagecache-6.x-2.0-beta12.tar.gz
+tar xvzf imagecache-6.x-2.0-beta12.tar.gz
+rm imagecache-6.x-2.0-beta12.tar.gz
+wget http://ftp.drupal.org/files/projects/imagefield-6.x-3.10.tar.gz
+tar xvzf imagefield-6.x-3.10.tar.gz
+rm imagefield-6.x-3.10.tar.gz
+wget http://ftp.drupal.org/files/projects/jquery_ui-6.x-1.5.tar.gz
+tar xvzf jquery_ui-6.x-1.5.tar.gz
+rm jquery_ui-6.x-1.5.tar.gz
+wget http://ftp.drupal.org/files/projects/pathauto-6.x-1.5.tar.gz
+tar xvzf pathauto-6.x-1.5.tar.gz
+rm pathauto-6.x-1.5.tar.gz
+wget http://ftp.drupal.org/files/projects/skinr-6.x-1.6.tar.gz
+tar xvzf skinr-6.x-1.6.tar.gz
+rm skinr-6.x-1.6.tar.gz
+wget http://ftp.drupal.org/files/projects/tagadelic-6.x-1.3.tar.gz
+tar xvzf tagadelic-6.x-1.3.tar.gz
+rm tagadelic-6.x-1.3.tar.gz
+wget http://ftp.drupal.org/files/projects/token-6.x-1.16.tar.gz
+tar xvzf token-6.x-1.16.tar.gz
+rm token-6.x-1.16.tar.gz
+wget http://ftp.drupal.org/files/projects/ubercart-6.x-2.4.tar.gz
+tar xvzf ubercart-6.x-2.4.tar.gz
+rm ubercart-6.x-2.4.tar.gz
+wget http://ftp.drupal.org/files/projects/views-6.x-2.12.tar.gz
+tar xvzf views-6.x-2.12.tar.gz
+rm views-6.x-2.12.tar.gz
+wget http://ftp.drupal.org/files/projects/webform-6.x-3.11.tar.gz
+tar xvzf webform-6.x-3.11.tar.gz
+rm webform-6.x-3.11.tar.gz
